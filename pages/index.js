c261a6e244c7f1b5a378070e01ec498ab798b1ea
CBTech1 <101056464+CBTech1@users.noreply.github.com>
2023-09-20 02:39:36 +0000

    I have added the boot strap and global css file to my Next.js app and I added some HTML to the app.

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+export default function Maison(){
+  return(
+    <h1>Salut mon amie, c'est Next.js ici. :)</h1>
+  );
+}
---
+++ pages/myApp.js
@@ -0,0 +1,10 @@
+import '../styles/globals.css';
+import '../styles/bootstrap.min.css';
+
+function monApp( {Component, pageProps} ){
+  return(
+    <Component {...pageProps}/>
+  );
+}
+
+export default monApp;
